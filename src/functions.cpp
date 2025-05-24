a628438f8875537eb49eadf9faacb6a449421673
Nikita Jegorov <proxjega@gmail.com>
2025-05-24 20:53:25 +0300

    quick fix

--- src/functions.cpp
+++ src/functions.cpp
@@ -15,7 +15,6 @@ void PrepareUrls(std::vector<std::wstring> &urls ) {
 		input >> url;
 		std::transform(url.begin(), url.end(), url.begin(), ::tolower);
 		url = L"." + url;
-		//std::wcout << L"URL: " << url << std::endl;
 		if (url.length() > 1) {
 			urls.push_back(url);
 		}
@@ -31,19 +30,15 @@ void FixWord(std::wstring& word) {
 		wchar_t c = word[i];
 		if (c == L'1' || c == L'2' || c == L'3' || c == L'4' || c == L'5' || c == L'6' || c == L'7' || c == L'8' || c == L'9' || c == L'0') {
 			word.clear();
-			//return word;
 		}
 		if (c == L'[') {
 			word = word.substr(0, i);
-			//return word;
 		}
 		if (c == L'/' || c == L'\\' || c == L':' || c == L'*' || c == L'?' || c == L'"' || c == L'<' || c == L'>' || c == L'|' || c == L'#'
 			|| c == L'.' || c == L',' || c == L'!' || c == L'@' || c == L'$' || c == L'%' || c == L'^' ||/* c == L'&' ||*/ c == L'(' || c == L')'
 			|| c == L'-' || c == L'=' || c == L'_' || c == L'„' || c == L'“' || c == L'–') {
 			word.erase(word.begin() + i);
-			//word.replace(i, 1, L" ");
 			i--;
 		}
 	}
-	//return word;
 }
